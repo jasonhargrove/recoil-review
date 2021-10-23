372763c41a5fd9a0c2eab904aa14024bb4b66887
Jason Hargrove <jasonhargrove1@gmail.com>
2021-10-23 13:25:41 -0700

    test drive recoil

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {
+  // RecoilRoot,
+  atom,
+  selector,
+  useRecoilState,
+  useRecoilValue
+} from 'recoil';
+
+const textState = atom({
+  key: 'textState',
+  default: new Date() + ''
+});
+
+const charCountState = selector({
+  key: 'charCountState',
+  get: ({get}) => {
+    const text = get(textState);
+    return text.length;
+  }
+});
 
 function App() {
+  const [text, setText] = useRecoilState(textState);
+  const count = useRecoilValue(charCountState);
+  setInterval(x => {
+    setText(new Date() + '');
+  }, 1000)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {text} {count}
     </div>
   );
 }
